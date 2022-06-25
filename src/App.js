6f61428ca9becf7eadbf05b9a5e8578adc0b50b1
DylanLovell93 <arcanyne7@gmail.com>
2022-06-25 16:50:18 -0400

    reservations path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LandingPage from "./Pages/LandingPage/LandingPage";
 import Restaurants from "./Pages/Restaurants/Restaurants";
 import NewRestaurant from "./Pages/NewRestaurant/NewRestaurant";
 import SingleRestaurant from "./Pages/SingleRestaurant/SingleRestaurant";
+import Reservations from "./Pages/Reservations/Reservations";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/restaurants" element={<Restaurants />} />
         <Route path="/newRestaurant" element={<NewRestaurant />} />
         <Route path="/restaurants/:id" element={<SingleRestaurant />} />
+        <Route path="/reservations" element={<Reservations />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/Reservations/Reservations.js
@@ -0,0 +1,8 @@
+import "./Reservations.css";
+import React from "react";
+
+const Reservations = () => {
+  return <div className="Reservations">Reservations</div>;
+};
+
+export default Reservations;
