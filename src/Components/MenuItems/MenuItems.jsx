af1ed7f596bacb62a8da35d081c0a2099470c202
DylanLovell93 <arcanyne7@gmail.com>
2022-06-27 08:36:51 -0400

    modal for menu navbad

---
+++ src/Components/MenuItems/MenuItems.jsx
@@ -0,0 +1,39 @@
+import "./MenuItems.css";
+import { IconButton, Typography, Toolbar } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { Link } from "react-router-dom";
+import itemData from "./itemData.js";
+
+const MenuItems = ({ onClose, mobile }) => {
+  const listItems = itemData.map((item) => (
+    <li>
+      <Link to={item.link}>
+        <Typography variant="h5" component="div" sx={{ mr: "auto" }}>
+          {item.text}
+        </Typography>
+      </Link>
+    </li>
+  ));
+  return (
+    <div className="MenuItems">
+      <Toolbar className="menuBar">
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ mx: "auto" }}>
+          Mobile
+        </Typography>
+        <div className="spacer"></div>
+      </Toolbar>
+      <ul>{listItems}</ul>
+    </div>
+  );
+};
+
+export default MenuItems;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import "./Navbar.css";
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -6,22 +7,57 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
+import Modal from "@mui/material/Modal";
+import MenuItems from "../MenuItems/MenuItems";
 
 const Navbar = () => {
+  const mobileQuery = window.matchMedia("(max-width: 600px)");
+  const [mobile, setMobile] = useState(mobileQuery.matches);
+  const [menuOpen, setMenu] = useState(false);
+  const [searchOpen, setSearch] = useState(false);
+
+  const handleMenuOpen = () => {
+    setMenu(true);
+  };
+
+  const handleMenuClose = () => {
+    setMenu(false);
+  };
+
+  const handleSearchOpen = () => {
+    setSearch(true);
+  };
+
+  const handleSearchClose = () => {
+    setSearch(false);
+  };
+
+  mobileQuery.onchange = (e) => {
+    setMobile(e.matches);
+  };
+
   return (
     <Box className="NavBar" sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="primary">
+      <AppBar position="static">
         <Toolbar>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
+            onClick={handleMenuOpen}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ mx: "auto" }}>
-            Name
+          <Modal open={menuOpen} onClose={handleMenuClose}>
+            <MenuItems onClose={handleMenuClose} mobile={mobile} />
+          </Modal>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={mobile ? { mx: "auto" } : { ml: 2, mr: "auto" }}
+          >
+            {mobile ? "Mobile" : "Desktop"}
           </Typography>
           <IconButton size="large" edge="end" color="inherit" aria-label="menu">
             <SearchIcon />
