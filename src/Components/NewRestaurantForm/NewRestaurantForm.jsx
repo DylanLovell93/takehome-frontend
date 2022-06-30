3b4c8d8129030f7c48ce489b1fd56550fc8233b5
DylanLovell93 <arcanyne7@gmail.com>
2022-06-29 22:14:45 -0400

    stopping point

--- src/Components/NewRestaurantForm/NewRestaurantForm.jsx
+++ src/Components/NewRestaurantForm/NewRestaurantForm.jsx
@@ -8,12 +8,52 @@ import {
   Box,
   Typography,
   TextField,
+  MenuItem,
 } from "@mui/material";
-import { orange } from "@mui/material/colors";
 
 const NewRestaurantForm = () => {
+  const sx = {
+    mx: "auto",
+    flexGrow: 1,
+    "& label.Mui-focused": {
+      color: "white",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "white",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "white",
+        borderWidth: "1.5px",
+      },
+    },
+    "& .MuiOutlinedInput-root:hover": {
+      "& fieldset": {
+        borderColor: "white",
+        borderWidth: "1.5px",
+      },
+    },
+    "& .MuiInputBase-formControl": {
+      color: "white",
+    },
+    "& .MuiInputLabel-root": {
+      color: "#606060",
+    },
+    "& .MuiOutlinedInput-root:focus-within": {
+      "& fieldset": {
+        borderColor: "white",
+        borderWidth: "1.5px",
+      },
+    },
+  };
+
+  const options = [
+    { value: "$", label: "$" },
+    { value: "$$", label: "$$" },
+    { value: "$$$", label: "$$$" },
+  ];
   return (
-    <Box className="NewRestaurantForm" component="form" autocomplete="off">
+    <Box className="NewRestaurantForm" component="form" autoComplete="off">
       <Typography
         variant="h5"
         component="div"
@@ -22,51 +62,32 @@ const NewRestaurantForm = () => {
         New Restaurant
       </Typography>
       <div className="formControl">
+        <TextField required id="name" label="Name" variant="outlined" sx={sx} />
+      </div>
+      <div className="formControl">
         <TextField
-          id="test"
-          label="Test"
+          required
+          id="description"
+          label="Description"
           variant="outlined"
-          sx={{
-            mx: "auto",
-            flexGrow: 1,
-            "& fieldset": {
-              borderColor: "white",
-            },
-            "&.Mui-focused fieldset": {
-              borderColor: "white",
-            },
-            "& label.Mui-focused": {
-              color: "white",
-            },
-            "& .MuiInput-underline:after": {
-              borderBottomColor: "white",
-            },
-            "& .MuiOutlinedInput-root": {
-              "& fieldset": {
-                borderColor: "white",
-                borderWidth: "1.5px",
-              },
-            },
-            "& .MuiOutlinedInput-root:hover": {
-              "& fieldset": {
-                borderColor: "white",
-                borderWidth: "2px",
-              },
-            },
-            "& .MuiInputBase-formControl": {
-              color: "white",
-            },
-            "& .MuiInputLabel-root": {
-              color: "white",
-            },
-            "& .MuiOutlinedInput-root:blur": {
-              "& fieldset": {
-                borderColor: "white",
-                borderWidth: "1.5px",
-              },
-            },
-          }}
+          sx={sx}
         />
+      </div>
+      <div className="formControl">
+        <TextField
+          required
+          select
+          id="price"
+          label="Price"
+          variant="outlined"
+          sx={sx}
+        >
+          {options.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
       </div>
     </Box>
   );
