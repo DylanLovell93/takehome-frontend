957ae9df08d5944d2f6c56ad615c576a9f287450
DylanLovell93 <arcanyne7@gmail.com>
2022-07-12 14:21:39 -0400

    error card message created

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,13 @@
 import "./ErrorPage.css";
 import React from "react";
 import NavBar from "../../Components/NavBar/NavBar";
+import ErrorCard from "../../Components/ErrorCard/ErrorCard";
 
 const ErrorPage = ({ mobile }) => {
   return (
     <div className="ErrorPage">
       <NavBar mobile={mobile} />
-      <div>Error</div>
+      <ErrorCard mobile={mobile} />
     </div>
   );
 };
