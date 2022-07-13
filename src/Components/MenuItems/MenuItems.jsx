11ad71a79d052ff53b63724e24557eafc36d8274
DylanLovell93 <arcanyne7@gmail.com>
2022-07-12 20:15:06 -0400

    icons for menu modal

--- src/Components/MenuItems/MenuItems.jsx
+++ src/Components/MenuItems/MenuItems.jsx
@@ -4,13 +4,31 @@ import React from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import { Link } from "react-router-dom";
 import itemData from "./itemData.js";
+import AddIcon from "@mui/icons-material/Add";
+import HomeIcon from "@mui/icons-material/Home";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 const MenuItems = React.forwardRef((props, ref) => {
   const { onClose } = props;
+
+  const iconKeys = {
+    home: <HomeIcon />,
+    food: <RestaurantIcon />,
+    plus: <AddIcon />,
+    calen: <CalendarMonthIcon />,
+  };
+
   const listItems = itemData.map((item, idx) => (
     <li key={"li" + (idx + 1)}>
       <Link to={item.link} onClick={onClose}>
-        <Typography variant="h5" component="div" sx={{ mr: "auto" }}>
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{ mr: "auto" }}
+          className="listItem"
+        >
+          {iconKeys[item.icon]}
           {item.text}
         </Typography>
       </Link>
